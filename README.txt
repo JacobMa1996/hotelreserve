155982fc2c07b0a8f5bc24b996c1392e60c7bf24
MatthewMaYao <934273746@qq.com>
2017-12-16 00:21:05 +0800

    update: 汇峰答辩

---
+++ README.txt
@@ -0,0 +1,4 @@
+windows环境，安装node
+cmd或git bash等命令行等进入根目录
+启动命令：supervisor ./bin/www
+浏览器访问localhost:3000
--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//app.get("#panorama1",routes.panorama1);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/users', users);
